233b28b35610f231f705eecbca2fe53d48c79037
Hasret Sariyer <hasretsariyer@gmail.com>
2020-06-03 23:21:40 +0300

    Add flutter build

--- main.rb
+++ main.rb
@@ -1,13 +1,17 @@
 require 'yaml'
 require 'open3'
+require 'pathname'
+require 'fileutils'
 
 def get_env_variable(key)
 	return (ENV[key] == nil || ENV[key] == "") ? nil : ENV[key]
 end
 
+ac_repo_path = get_env_variable("AC_REPOSITORY_DIR") || abort('Missing repo path.')
 ac_output_type = get_env_variable("AC_OUTPUT_TYPE") || abort('Missing output type.')
-ac_flutter_build_mode = get_env_variable("AC_FLUTTER_BUILD_MODE") || abort('Missing flutter build mode.')
+ac_output_folder = get_env_variable("AC_OUTPUT_DIR") || abort('Missing output folder.')
 ac_flutter_build_extra_args = get_env_variable("AC_FLUTTER_BUILD_EXTRA_ARGS") || ""
+ac_flutter_build_mode = get_env_variable("AC_FLUTTER_BUILD_MODE") || "release"
 ac_project_path = get_env_variable("AC_PROJECT_PATH") || "."
 
 def run_command(command)
@@ -33,11 +37,19 @@ def run_command(command)
 end
 
 build_type = (ac_output_type == "aab") ? "appbundle" : "apk"
-run_command("cd #{ac_project_path} && flutter build #{build_type} #{ac_flutter_build_extra_args} --#{ac_flutter_build_mode}")
 
-build_outputs_folder = "#{ENV["AC_REPOSITORY_DIR"]}/build/app/outputs"
-apk_filter_pattern = "#{build_outputs_folder}/apk/**/*.apk"
-aab_filter_pattern = "#{build_outputs_folder}/bundle/**/*.apk"
+project_root_path = ""
+if Pathname.new("#{ac_project_path}").absolute?
+    project_root_path = ac_project_path
+else
+    project_root_path = File.expand_path(File.join(ac_repo_path, ac_project_path))
+end
+
+run_command("cd #{project_root_path} && flutter build #{build_type} #{ac_flutter_build_extra_args} --#{ac_flutter_build_mode}")
+
+build_outputs_folder = "#{ac_repo_path}/build/app/outputs"
+apk_filter_pattern = "#{build_outputs_folder}/apk/***/*.apk"
+aab_filter_pattern = "#{build_outputs_folder}/bundle/***/*.apk"
 
 puts "Filtering artifacts: #{apk_filter_pattern}, #{aab_filter_pattern}"
 
